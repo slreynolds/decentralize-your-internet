fe97d0a141bdab9378bbe5eb42bebf5e107c72d9
schlotkrabbe <mirringandreas@gmail.com>
2024-12-24 22:19:02 +0100

    bug: fix jenkins stuff

--- scripts/step_5/prep_nextcloud_env.sh
+++ scripts/step_5/prep_nextcloud_env.sh
@@ -1,5 +1,8 @@
 echo "creating folders"
-cd /home/cloudy
+HOME_DIR="/home/cloudy"
+KEY_NAME="jenkins_deploy_key"
+
+cd ""$HOME_DIR""
 mkdir nextcloud_data
 mkdir nextcloud_logs
 mkdir nextcloud_ssl
@@ -9,8 +12,11 @@ mkdir nextcloud_keys
 echo "copying infra files"
 cp decentralize-your-internet/infra/nextcloud/compose.yml nextcloud_infra/
 cp decentralize-your-internet/infra/nextcloud/.env.example nextcloud_infra/.env
+
 echo "create deploy key"
-ssh-keygen -t rsa -b 4096 -f "$KEY_DIR/$KEY_NAME" -N "" -q
-echo "Private key: $KEY_DIR/$KEY_NAME"
-echo "Public key: $KEY_DIR/$KEY_NAME.pub"
+ssh-keygen -t rsa -b 4096 -f "$HOME_DIR/$KEY_NAME" -N "" -q
+echo "Private key: $HOME_DIR/$KEY_NAME"
+echo "Public key: $HOME_DIR/$KEY_NAME.pub"
+
 cd nextcloud_infra
+echo "!!! check the .env and adjust secrets and project names if necessary !!!"
