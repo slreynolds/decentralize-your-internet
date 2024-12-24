2ec760a04e87c36314183607c1eea840ecced27a
schlotkrabbe <mirringandreas@gmail.com>
2024-12-24 06:35:16 +0100

    bug: fix step5 script

--- scripts/step_5/prep_nextcloud_env.sh
+++ scripts/step_5/prep_nextcloud_env.sh
@@ -5,6 +5,6 @@ mkdir nextcloud_logs
 mkdir nextcloud_ssl
 mkdir nextcloud_html
 mkdir nextcloud_infra
-cp decentralize-your-internet/infra/nextcloud/docker_compose.yml nextcloud_infra/
+cp decentralize-your-internet/infra/nextcloud/compose.yml nextcloud_infra/
 cp decentralize-your-internet/infra/nextcloud/.env.example nextcloud_infra/.env
 cd nextcloud_infra
